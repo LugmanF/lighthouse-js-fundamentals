0ab95cd8e935b9985078d210bce21bdcaa45cea0
LugmanF <farahlugman@gmail.com>
2022-04-19 21:38:25 +0000

    changes to function example

--- hello.js
+++ hello.js
@@ -6,4 +6,14 @@ sayHello("Cali");
 sayHello("Sophia");
 sayHello("Sarah");
 
+const sayHelloToConsole = function (name) {
+console.log("Hello, " + name);
+}
+sayHelloToConsole('Jim');
+
+const returnSayHello = function (name) {
+return "Hello, " + name;
+}
+const greeting = returnSayHello('Jim');
+console.log(greeting);
 

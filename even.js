e9f4aa0b9db68c107cf8f809b29096e2b9ff1594
LugmanF <farahlugman@gmail.com>
2022-04-19 21:49:17 +0000

    Adding inequality operators to functions

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0; 
+}
+console.log(isEven(10));
+console.log(isEven(11));
+
+const isOdd = function (number) {
+return number % 2 !== 0;
+}
+console.log(isOdd(9));
+console.log(isOdd(10));
+ 
